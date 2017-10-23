44b90cde5bdc1f04f4189c22ed20881517b693e3
Otavio Salvador <otavio@ossystems.com.br>
2017-10-23 20:33:32 -0200

    Use a m4 sub-directory for m4 files
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,9 @@
 
 echo "Generating configure files... may take a while."
 
+# Silent warning about missing directory
+mkdir -p m4
+
 autoreconf --install --force && \
   echo "Preparing was successful if there was no error messages above." && \
   echo "Now type:" && \
