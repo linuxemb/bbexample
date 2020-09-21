9a4aee78cb5735eb14ed7d0fb2930d138b60f8cd
linuxemb <itss07@gmail.com>
2020-09-21 16:38:05 -0400

    test autorev 3nd time

--- bbexample.c
+++ bbexample.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
   printf("Hello Yocto World. test sstate cache..\n");
   printf("Hello Yocto World. test autorev.\n");
   printf("Hello Yocto World. test autorev 2nd time.\n");
+  printf("Hello Yocto World. test autorev 3rd time.\n");
 
   LibHelloWorld();
 
